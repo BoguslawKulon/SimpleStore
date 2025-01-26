35d093459420b2292061eaea2b8aeb2852c0aee6
Bogusław Kulon <bogkul@gmail.com>
2025-01-26 20:14:43 +0100

    Add ViewModels

---
+++ src/UserManagement/ViewModels/UserLoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.ViewModels;
+public class UserLoginVM
+{
+    public int Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    [MinLength(8), MaxLength(30)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,30}$")]
+    public required string Password { get; set; }
+}
---
+++ src/UserManagement/ViewModels/UserRegistrationVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.ViewModels;
+public class UserRegistrationVM
+{
+    private const string PASSWORD_MISMATCH_ERROR_MESSAGE = "Password are different";
+
+    public int Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    [MinLength(8), MaxLength(30)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,30}$")]
+    public string Password { get; set; }
+
+    [Compare(nameof(Password), ErrorMessage = PASSWORD_MISMATCH_ERROR_MESSAGE)]
+    public string SecondPassword { get; set; }
+}
